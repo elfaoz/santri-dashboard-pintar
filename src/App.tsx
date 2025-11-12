a53c998033203296459a478526661ffbffba342e
elfaoz <nashers.manziel@gmail.com>
2025-11-12 23:41:13 +0700

    App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
@@ -11,7 +10,10 @@ import { MemorizationProvider } from "@/contexts/MemorizationContext";
 import { AttendanceProvider } from "@/contexts/AttendanceContext";
 import { ActivityProvider } from "@/contexts/ActivityContext";
 import { FinanceProvider } from "@/contexts/FinanceContext";
+
 import Layout from "./components/Layout";
+import { PWAInstallPrompt } from "./components/PWAInstallPrompt";
+
 import Home from "./pages/Home";
 import Dashboard from "./pages/Dashboard";
 import Profile from "./pages/Profile";
@@ -41,70 +43,97 @@ const App = () => (
               <ActivityProvider>
                 <FinanceProvider>
                   <TooltipProvider>
-            <Toaster />
-            <Sonner />
-            <BrowserRouter>
-              <Routes>
-                <Route path="/" element={<Home />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/signup" element={<SignUp />} />
-            <Route path="/kdm" element={<KDM />} />
-            <Route path="/upgrade" element={<UpgradePlan />} />
-            <Route path="/payment" element={<Payment />} />
-            <Route path="/install" element={<Install />} />
-                <Route path="/dashboard" element={
-                  <ProtectedRoute>
-                    <Layout>
-                      <Dashboard />
-                    </Layout>
-                  </ProtectedRoute>
-                } />
-                <Route path="/profile" element={
-                  <ProtectedRoute>
-                    <Layout>
-                      <Profile />
-                    </Layout>
-                  </ProtectedRoute>
-                } />
-                <Route path="/attendance" element={
-                  <ProtectedRoute>
-                    <Layout>
-                      <Attendance />
-                    </Layout>
-                  </ProtectedRoute>
-                } />
-                <Route path="/halaqah" element={
-                  <ProtectedRoute>
-                    <Layout>
-                      <Halaqah />
-                    </Layout>
-                  </ProtectedRoute>
-                } />
-                <Route path="/activities" element={
-                  <ProtectedRoute>
-                    <Layout>
-                      <Activities />
-                    </Layout>
-                  </ProtectedRoute>
-                } />
-                <Route path="/finance" element={
-                  <ProtectedRoute>
-                    <Layout>
-                      <Finance />
-                    </Layout>
-                  </ProtectedRoute>
-                } />
-                <Route path="/add-student" element={
-                  <ProtectedRoute>
-                    <Layout>
-                      <AddStudent />
-                    </Layout>
-                  </ProtectedRoute>
-                } />
-                <Route path="*" element={<NotFound />} />
-              </Routes>
-            </BrowserRouter>
-          </TooltipProvider>
+                    <Toaster />
+                    <Sonner />
+                    <BrowserRouter>
+                      {/* ✅ Komponen PWAInstallPrompt hanya muncul di home & /install */}
+                      <PWAInstallPrompt />
+                      
+                      <Routes>
+                        {/* Halaman publik */}
+                        <Route path="/" element={<Home />} />
+                        <Route path="/login" element={<Login />} />
+                        <Route path="/signup" element={<SignUp />} />
+                        <Route path="/kdm" element={<KDM />} />
+                        <Route path="/upgrade" element={<UpgradePlan />} />
+                        <Route path="/payment" element={<Payment />} />
+                        <Route path="/install" element={<Install />} />
+
+                        {/* Halaman yang dilindungi */}
+                        <Route
+                          path="/dashboard"
+                          element={
+                            <ProtectedRoute>
+                              <Layout>
+                                <Dashboard />
+                              </Layout>
+                            </ProtectedRoute>
+                          }
+                        />
+                        <Route
+                          path="/profile"
+                          element={
+                            <ProtectedRoute>
+                              <Layout>
+                                <Profile />
+                              </Layout>
+                            </ProtectedRoute>
+                          }
+                        />
+                        <Route
+                          path="/attendance"
+                          element={
+                            <ProtectedRoute>
+                              <Layout>
+                                <Attendance />
+                              </Layout>
+                            </ProtectedRoute>
+                          }
+                        />
+                        <Route
+                          path="/halaqah"
+                          element={
+                            <ProtectedRoute>
+                              <Layout>
+                                <Halaqah />
+                              </Layout>
+                            </ProtectedRoute>
+                          }
+                        />
+                        <Route
+                          path="/activities"
+                          element={
+                            <ProtectedRoute>
+                              <Layout>
+                                <Activities />
+                              </Layout>
+                            </ProtectedRoute>
+                          }
+                        />
+                        <Route
+                          path="/finance"
+                          element={
+                            <ProtectedRoute>
+                              <Layout>
+                                <Finance />
+                              </Layout>
+                            </ProtectedRoute>
+                          }
+                        />
+                        <Route
+                          path="/add-student"
+                          element={
+                            <ProtectedRoute>
+                              <Layout>
+                                <AddStudent />
+                              </Layout>
+                            </ProtectedRoute>
+                          }
+                        />
+                        <Route path="*" element={<NotFound />} />
+                      </Routes>
+                    </BrowserRouter>
+                  </TooltipProvider>
                 </FinanceProvider>
               </ActivityProvider>
             </AttendanceProvider>
